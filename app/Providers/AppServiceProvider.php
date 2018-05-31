f44fcda32a6643f66c5480abe3479988688164d2
toru.inukai <ec2-user@ip-172-31-21-168.us-east-2.compute.internal>
2018-05-31 05:47:54 +0000

    message model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,11 +11,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+ public function boot()
     {
-        //
+        \Schema::defaultStringLength(191);
     }
-
     /**
      * Register any application services.
      *
