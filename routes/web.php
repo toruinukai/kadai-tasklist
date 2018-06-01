14708e2ebfd139a70478bf06d63b20e1db609e51
toru.inukai <ec2-user@ip-172-31-21-168.us-east-2.compute.internal>
2018-06-01 03:34:50 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasklistsController@index');
+
+Route::resource('tasklists', 'TasklistsController');
+
+?>
